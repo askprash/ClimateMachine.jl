0a23786bcb658d6a5c60acc55a36bf281bdd9265
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-10-21 10:36:55 -0700

    Prep surface fluxes for gpu
    WIP

--- src/Common/SurfaceFluxes/SurfaceFluxes.jl
+++ src/Common/SurfaceFluxes/SurfaceFluxes.jl
@@ -26,6 +26,7 @@
 module SurfaceFluxes
 
 using NLsolve
+using KernelAbstractions: @print
 
 using ..Thermodynamics
 using DocStringExtensions
@@ -54,13 +55,18 @@ Surface flux conditions, returned from `surface_conditions`.
 
 $(DocStringExtensions.FIELDS)
 """
-struct SurfaceFluxConditions{FT}
+struct SurfaceFluxConditions{FT, VFT}
     L_MO::FT
     VDSE_flux_star::FT
-    flux::AbstractVector
-    x_star::AbstractVector
-    K_exchange::AbstractVector
+    flux::VFT
+    x_star::VFT
+    K_exchange::VFT
 end
+
+similar_instance(x::NTuple, t::Tuple) = x
+similar_instance(x::NTuple, t::SVector) = SVector(x...)
+similar_instance(x::NTuple, t::MArray{S, T}) where {S, T} = MArray{S, T}(x...)
+similar_instance(x::NTuple, t::Array) = collect(x)
 
 function Base.show(io::IO, sfc::SurfaceFluxConditions)
     println(io, "----------------------- SurfaceFluxConditions")
@@ -70,6 +76,49 @@ function Base.show(io::IO, sfc::SurfaceFluxConditions)
     println(io, "x_star         = ", sfc.x_star)
     println(io, "K_exchange     = ", sfc.K_exchange)
     println(io, "-----------------------")
+end
+
+function surface_fluxes_f!(F, x, nt::NamedTuple)
+    param_set = nt.param_set
+    VDSE_flux_star = nt.VDSE_flux_star
+    Δz = nt.Δz
+    z_0 = Tuple(nt.z_0)
+    x_ave = Tuple(nt.x_ave)
+    x_s = Tuple(nt.x_s)
+    n_vars = nt.n_vars
+    scheme = nt.scheme
+    VDSE_scale = nt.VDSE_scale
+
+    x_tup = Tuple(x)
+
+    u, θ = x_tup[2], x_tup[3]
+    if VDSE_flux_star == nothing
+        flux = -u * θ
+    else
+        flux = VDSE_flux_star
+    end
+    L_MO = monin_obukhov_length(param_set, u, VDSE_scale, flux)
+    uf = Businger(param_set, L_MO)
+    F_nt = ntuple(Val(n_vars + 1)) do i
+        if i == 1
+            F_i = x_tup[1] - monin_obukhov_length(param_set, u, VDSE_scale, flux)
+        else
+            ϕ = x_tup[i]
+            transport = i - 1 == 1 ? MomentumTransport() : HeatTransport()
+            F_i =
+                ϕ - compute_physical_scale(
+                    uf,
+                    Δz,
+                    z_0[i - 1],
+                    x_ave[i - 1],
+                    x_s[i - 1],
+                    transport,
+                    scheme,
+                )
+        end
+        F_i
+    end
+    F .= F_nt
 end
 
 """
@@ -84,7 +133,7 @@ Surface conditions given
  - `Δz` layer thickness (not spatial discretization)
  - `z` coordinate axis
  - `a` free model parameter with prescribed value of 4.7
- - `VDSE_flux_star_given` potential temperature flux (optional)
+ - `VDSE_flux_star` potential temperature flux (optional)
 
 If `VDSE_flux_star` is not given, then it is computed by iteration
 of equations 3, 17, and 18 in Nishizawa2018.
@@ -102,7 +151,7 @@ function surface_conditions(
     z::FT,
     a::FT,
     scheme,
-    VDSE_flux_star_given::Union{Nothing, FT} = nothing,
+    VDSE_flux_star::Union{Nothing, FT} = nothing,
 ) where {FT <: AbstractFloat, AbstractEarthParameterSet}
 
     n_vars = length(x_initial) - 1
@@ -112,39 +161,29 @@ function surface_conditions(
     @assert length(z_0) == n_vars
     @assert length(F_exchange) == n_vars
     local sol
-    u, θ, qt = x_initial[1], x_initial[2], x_initial[3]
-    let param_set = param_set
-        function f!(F, x_all)
-            L_MO, x_vec = x_all[1], x_all[2:end]
-            u, θ = x_vec[1], x_vec[2]
-            VDSE_flux_star =
-                VDSE_flux_star_given == nothing ? -u * θ : VDSE_flux_star_given
-            flux = VDSE_flux_star
-            F[1] = L_MO - monin_obukhov_length(param_set, u, VDSE_scale, flux)
-            for i in 1:n_vars
-                ϕ = x_vec[i]
-                transport = i == 1 ? MomentumTransport() : HeatTransport()
-                L_MO = monin_obukhov_length(param_set, u, VDSE_scale, flux)
-                uf = Businger(param_set, L_MO)
-                F[i + 1] =
-                    ϕ - compute_physical_scale(
-                        uf,
-                        Δz,
-                        z_0[i],
-                        x_ave[i],
-                        x_s[i],
-                        transport,
-                        scheme,
-                    )
-            end
-        end
-        sol = nlsolve(f!, x_initial, autodiff = :forward)
+
+    args = (
+        param_set = param_set,
+        VDSE_flux_star = VDSE_flux_star,
+        Δz = Δz,
+        z_0 = z_0,
+        n_vars = n_vars,
+        x_ave = x_ave,
+        x_s = x_s,
+        scheme = scheme,
+        VDSE_scale = VDSE_scale,
+        x_initial = x_initial,
+    )
+    let args = args
+        # Define closure over args
+        f!(F, x_all) = surface_fluxes_f!(F, x_all, args)
+        sol = nlsolve(f!, args.x_initial, autodiff = :forward)
     end
     if converged(sol)
         L_MO, x_star = sol.zero[1], collect(sol.zero[2:end])
         u_star, θ_star = x_star[1], x_star[2]
     else
-        #println("Warning: Unconverged Surface Fluxes")
+        @print("Warning: Unconverged Surface Fluxes\n")
         L_MO, x_star = sol.zero[1], sol.zero[2:end]
         u_star, θ_star = x_star[1], x_star[2]
     end
@@ -167,7 +206,14 @@ function surface_conditions(
     C_exchange =
         get_flux_coefficients(param_set, z, a, x_star, VDSE_scale, L_MO, z_0)
 
-    return SurfaceFluxConditions(L_MO, VDSE_flux_star, flux, x_star, C_exchange)
+    VFT = typeof(flux)
+    return SurfaceFluxConditions{FT, VFT}(
+        L_MO,
+        VDSE_flux_star,
+        flux,
+        x_star,
+        C_exchange,
+    )
 end
 
 """
@@ -244,45 +290,52 @@ function exchange_coefficients(
     z,
     F_exchange,
     a,
-    x_star,
+    x_star::VFT,
     VDSE_scale,
     L_MO,
-)
+) where {VFT}
     N = length(F_exchange)
     FT = typeof(z)
     _von_karman_const::FT = von_karman_const(param_set)
-    K_exchange = zeros(length(x_star))
     uf = Businger(param_set, L_MO)
-    for i in 1:N
+
+    K_exchange = ntuple(Val(length(x_star))) do i
         transport = i == 1 ? MomentumTransport() : HeatTransport()
         phi_t = phi(uf, z / L_MO, transport)
         _π_group = FT(UF.π_group(uf, transport))
-        K_exchange[i] =
-            -F_exchange[i] * _von_karman_const * z / _π_group /
-            (x_star[i] * phi_t) # Eq. 19 in
+        num = -F_exchange[i] * _von_karman_const * z
+        den = _π_group * (x_star[i] * phi_t)
+        K_exch = num / den # Eq. 19 in
     end
-    return K_exchange
+    return similar_instance(K_exchange, x_star)
 end
 
-function get_flux_coefficients(param_set, z, a, x_star, VDSE_scale, L_MO, z0)
+function get_flux_coefficients(
+    param_set,
+    z,
+    a,
+    x_star::VFT,
+    VDSE_scale,
+    L_MO,
+    z0,
+) where {VFT}
     N = length(x_star)
     FT = typeof(z)
     _von_karman_const::FT = von_karman_const(param_set)
-    C = zeros(length(x_star))
     uf = Businger(param_set, L_MO)
     psi_m = psi(uf, z / L_MO, MomentumTransport())
     psi_h = psi(uf, z / L_MO, HeatTransport())
-    for i in 1:N
+    C = ntuple(Val(length(x_star))) do i
         logζ_ψ_m = log(z / z0[i] - psi_m)
         logζ_ψ_h = log(z / z0[i] - psi_h)
         if i == 1
-            C[i] = _von_karman_const^2 / logζ_ψ_m^2
+            C_i = _von_karman_const^2 / logζ_ψ_m^2
         else
-            C[i] = _von_karman_const^2 / logζ_ψ_m / logζ_ψ_h
+            C_i = _von_karman_const^2 / logζ_ψ_m / logζ_ψ_h
         end
+        C_i
     end
-    return (C)
-
+    return similar_instance(C, x_star)
 end
 
 end # SurfaceFluxes module
