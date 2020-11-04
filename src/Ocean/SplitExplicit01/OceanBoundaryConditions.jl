9a74253ebdcd2cefda3f0c270710c7312f712a7a
christophernhill <cnh@mit.edu>
2020-11-04 12:00:31 -0500

    Format base files in CLiMA style  o fixed base files in ocean/dev needed climaformat.jl to pass CI
     o fixed pipeline.yml to pass CI with separate branch from master
       some pipeline.yml tests are commented out for now
    
     o added one missing docstring entry, for new timestepper, to Ocean.md

--- src/Ocean/SplitExplicit01/OceanBoundaryConditions.jl
+++ src/Ocean/SplitExplicit01/OceanBoundaryConditions.jl
@@ -129,9 +129,9 @@ apply no penetration boundary for temperature
     A⁻,
     t,
 )
-#   D⁺.ν∇u = -D⁻.ν∇u
-#   D⁺.κ∇θ = -D⁻.κ∇θ
-#-  new diffusive flux BC:
+    #   D⁺.ν∇u = -D⁻.ν∇u
+    #   D⁺.κ∇θ = -D⁻.κ∇θ
+    #-  new diffusive flux BC:
     Q⁺.u = Q⁻.u
     A⁺.u_d = A⁻.u_d
     A⁺.w = A⁻.w
@@ -155,8 +155,8 @@ end
     A⁻,
     t,
 )
-#   D⁺.ν∇U = -D⁻.ν∇U
-#-  new diffusive flux BC:
+    #   D⁺.ν∇U = -D⁻.ν∇U
+    #-  new diffusive flux BC:
     Q⁺.U = Q⁻.U
     D⁺.ν∇U = n⁻ * (@SVector [-0, -0])'
 
@@ -270,11 +270,11 @@ apply no penetration boundary for temperature
     Q⁺.u = -Q⁻.u
     A⁺.u_d = -A⁻.u_d
 
-#   D⁺.κ∇θ = -D⁻.κ∇θ
+    #   D⁺.κ∇θ = -D⁻.κ∇θ
 
-#-  new diffusive flux BC:
-#   Q⁺.u = -Q⁻.u
-#   A⁺.w = -A⁻.w
+    #-  new diffusive flux BC:
+    #   Q⁺.u = -Q⁻.u
+    #   A⁺.w = -A⁻.w
     Q⁺.θ = Q⁻.θ
     D⁺.ν∇u = D⁻.ν∇u
     D⁺.κ∇θ = n⁻ * -0
@@ -296,7 +296,7 @@ end
     t,
 )
     Q⁺.U = -Q⁻.U
-#-  new diffusive flux BC:
+    #-  new diffusive flux BC:
     D⁺.ν∇U = D⁻.ν∇U
 
     return nothing
@@ -372,12 +372,12 @@ apply no penetration boundary for temperature
     A⁻,
     t,
 )
-#   A⁺.w = -A⁻.w
-#   D⁺.ν∇u = -D⁻.ν∇u
+    #   A⁺.w = -A⁻.w
+    #   D⁺.ν∇u = -D⁻.ν∇u
 
-#   D⁺.κ∇θ = -D⁻.κ∇θ
+    #   D⁺.κ∇θ = -D⁻.κ∇θ
 
-#-  new diffusive flux BC:
+    #-  new diffusive flux BC:
     Q⁺.u = Q⁻.u
     A⁺.u_d = A⁻.u_d
     A⁺.w = A⁻.w
@@ -464,11 +464,11 @@ apply no penetration boundary for temperature
     Q⁺.u = -Q⁻.u
     A⁺.w = -A⁻.w
 
-#   D⁺.κ∇θ = -D⁻.κ∇θ
+    #   D⁺.κ∇θ = -D⁻.κ∇θ
 
-#-  new diffusive flux BC:
-#   Q⁺.u = -Q⁻.u
-#   A⁺.w = -A⁻.w
+    #-  new diffusive flux BC:
+    #   Q⁺.u = -Q⁻.u
+    #   A⁺.w = -A⁻.w
     Q⁺.θ = Q⁻.θ
     D⁺.ν∇u = D⁻.ν∇u
     D⁺.κ∇θ = n⁻ * -0
@@ -519,11 +519,11 @@ apply no flux boundary condition for temperature
     A⁻,
     t,
 )
-#   D⁺.ν∇u = -D⁻.ν∇u
+    #   D⁺.ν∇u = -D⁻.ν∇u
 
-#   D⁺.κ∇θ = -D⁻.κ∇θ
+    #   D⁺.κ∇θ = -D⁻.κ∇θ
 
-#-  new diffusive flux BC:
+    #-  new diffusive flux BC:
     Q⁺.u = Q⁻.u
     A⁺.u_d = A⁻.u_d
     A⁺.w = A⁻.w
@@ -554,12 +554,12 @@ apply no flux boundary condition for temperature
     t,
 )
     τᶻ = velocity_flux(m.problem, A⁻.y, m.ρₒ)
-#   τ = @SMatrix [-0 -0; -0 -0; τᶻ -0]
-#   D⁺.ν∇u = -D⁻.ν∇u + 2 * τ
+    #   τ = @SMatrix [-0 -0; -0 -0; τᶻ -0]
+    #   D⁺.ν∇u = -D⁻.ν∇u + 2 * τ
 
-#   D⁺.κ∇θ = -D⁻.κ∇θ
+    #   D⁺.κ∇θ = -D⁻.κ∇θ
 
-#-  new diffusive flux BC:
+    #-  new diffusive flux BC:
     Q⁺.u = Q⁻.u
     A⁺.u_d = A⁻.u_d
     A⁺.w = A⁻.w
@@ -589,13 +589,13 @@ apply forcing boundary condition for temperature
     A⁻,
     t,
 )
-#   D⁺.ν∇u = -D⁻.ν∇u
+    #   D⁺.ν∇u = -D⁻.ν∇u
 
     σᶻ = temperature_flux(m.problem, A⁻.y, Q⁻.θ)
-#   σ = @SVector [-0, -0, σᶻ]
-#   D⁺.κ∇θ = -D⁻.κ∇θ + 2 * σ
+    #   σ = @SVector [-0, -0, σᶻ]
+    #   D⁺.κ∇θ = -D⁻.κ∇θ + 2 * σ
 
-#-  new diffusive flux BC:
+    #-  new diffusive flux BC:
     Q⁺.u = Q⁻.u
     A⁺.u_d = A⁻.u_d
     A⁺.w = A⁻.w
@@ -626,14 +626,14 @@ apply forcing boundary condition for temperature
     t,
 )
     τᶻ = velocity_flux(m.problem, A⁻.y, m.ρₒ)
-#   τ = @SMatrix [-0 -0; -0 -0; τᶻ -0]
-#   D⁺.ν∇u = -D⁻.ν∇u + 2 * τ
+    #   τ = @SMatrix [-0 -0; -0 -0; τᶻ -0]
+    #   D⁺.ν∇u = -D⁻.ν∇u + 2 * τ
 
     σᶻ = temperature_flux(m.problem, A⁻.y, Q⁻.θ)
-#   σ = @SVector [-0, -0, σᶻ]
-#   D⁺.κ∇θ = -D⁻.κ∇θ + 2 * σ
+    #   σ = @SVector [-0, -0, σᶻ]
+    #   D⁺.κ∇θ = -D⁻.κ∇θ + 2 * σ
 
-#-  new diffusive flux BC:
+    #-  new diffusive flux BC:
     Q⁺.u = Q⁻.u
     A⁺.u_d = A⁻.u_d
     A⁺.w = A⁻.w
