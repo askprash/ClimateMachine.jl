9c05d4d5a4551648597054087fd4667ec6553483
Gregory Wagner <wagner.greg@gmail.com>
2020-11-04 20:50:15 -0500

    Adds a test for InitialValueProblem plus formatting update

--- src/Ocean/Ocean.jl
+++ src/Ocean/Ocean.jl
@@ -34,8 +34,8 @@ include("OceanBC.jl")
 include("HydrostaticBoussinesq/HydrostaticBoussinesqModel.jl")
 include("ShallowWater/ShallowWaterModel.jl")
 include("SplitExplicit/SplitExplicitModel.jl")
-include("OceanProblems/OceanProblems.jl")
 include("SplitExplicit01/SplitExplicitModel.jl")
 include("OceanProblems/SimpleBoxProblem.jl")
+include("OceanProblems/OceanProblems.jl")
 
 end
